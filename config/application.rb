4a6f5e7e372f4bb276aa97c98af20631d8f8347e
Jason Kwong <jasonkwong11@gmail.com>
2017-10-24 20:27:23 -0400

    update dotenv requirement

--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-require 'dotenv'
-Dotenv.load
-
-
+require 'dotenv/load' if (ENV['RUBY_ENV'] == "development" || ENV['RUBY_ENV'] == "test")
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
