0edbbef2787480f44519522222f9a57bbd7140da
Jason Kwong <jasonkwong11@gmail.com>
2017-10-24 20:20:59 -0400

    init commit

---
+++ app/controllers/booking_controller.rb
@@ -0,0 +1,8 @@
+class BookingController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+end
---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+require 'dotenv'
+Dotenv.load
+
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module PayJason
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+    config.stripe.publishable_key = "pk_test_yttpe3MBKzBbISq3m3ziVBkh"
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
