d472fc4a47e82996b7a1fe57447e4e45d53233e1
Jason Kwong <jasonkwong11@gmail.com>
2017-10-28 14:15:14 -0400

    initial alipay integration

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -22,4 +22,30 @@ class ChargesController < ApplicationController
     flash[:error] = e.message
     redirect_to new_charge_path
   end
+
+  def alipay
+    source = Stripe::Source.create(
+      type: "alipay",
+      amount: 8000,
+      currency: 'usd',
+      redirect: {
+        return_url: 'http://localhost:3000/confirmation'
+      }
+    )
+    #if customer autohrizes the payment, the source
+    #object's status will transition to chargeable when
+    #it is ready to be used in a charge request
+    # ... if customer declines, status will transition to failed
+
+    redirect_to source[:redirect][:url]
+  end
+
+  def confirmation
+    #here the get request is returned with these
+    #params: source, livemode, client_secret
+    # you can include other params if you need
+    params
+    require 'pry'
+    binding.pry
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'booking#index'
+  root to: 'booking', to: 'booking#index'
   resources :booking
   resources :charges
+
+  get 'alipay', to: 'charges#alipay'
+  get 'confirmation', to: 'charges#confirmation'
 end
